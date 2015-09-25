9f48c564e78a956783bb87068d7e147791ab19f5
Tao Wang <tao.wang@utsouthwestern.edu>
2015-09-25 08:39:06 +0000

    version 1.2

---
+++ R/summarize_mat.R
@@ -0,0 +1,23 @@
+summarize_mat<-function(mat_main,ann_main,mat_supp,ann_supp,n_limit=50,extend=100000)
+{
+  mat_main=as.matrix(mat_main)
+  mod_main=mat_main
+  mat_supp=as.matrix(mat_supp)
+  
+  for (i in 1:dim(mat_main)[1]) 
+  {
+    ids=get_nearby(i,ann_supp,ann_main,n_limit=n_limit,extend=extend)
+    if (length(ids)==0) {next}
+    if (length(ids)==1)
+    {
+      PCA=mat_supp[ids,]
+    }else
+    {
+      PCA=prcomp(t(mat_supp[ids,]),.scale=T)$x[,1]
+    }
+    if (cor(PCA,mat_main[i,])<0) {PCA=-PCA}
+    mod_main[i,]=PCA
+  }
+  
+  mod_main
+}
