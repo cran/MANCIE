543e91218c92a7e250503fe7e36b6d78360d5bfc
Tao Wang <tao.wang@utsouthwestern.edu>
2016-03-02 01:14:19 +0000

    version 1.4

--- R/summarize_mat.R
+++ R/summarize_mat.R
@@ -1,4 +1,4 @@
-summarize_mat<-function(mat_main,ann_main,mat_supp,ann_supp,n_limit=50,extend=100000)
+summarize_mat<-function(mat_main,ann_main,mat_supp,ann_supp,n_limit=50,extend=100000,method="pca")
 {
   mat_main=as.matrix(mat_main)
   mod_main=mat_main
@@ -13,7 +13,14 @@ summarize_mat<-function(mat_main,ann_main,mat_supp,ann_supp,n_limit=50,extend=10
       PCA=mat_supp[ids,]
     }else
     {
-      PCA=prcomp(t(mat_supp[ids,]),.scale=T)$x[,1]
+      if (method=="max")
+      {
+        ids_max=ids[which.max(abs(cor(mat_main[i,],t(mat_supp[ids,]))))]
+        PCA=mat_supp[ids_max,]
+      }else
+      {
+        PCA=prcomp(t(mat_supp[ids,]),.scale=T)$x[,1]
+      }
     }
     if (cor(PCA,mat_main[i,])<0) {PCA=-PCA}
     mod_main[i,]=PCA
